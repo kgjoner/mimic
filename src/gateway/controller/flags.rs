b904c0b78cd18610ba20581ee86da05b509832be
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2023-05-14 19:59:44 -0300

    chore: create flag controller

---
+++ src/gateway/controller/flags.rs
@@ -0,0 +1,66 @@
+use std::collections::HashMap;
+
+use crate::gateway::presenter::NormalizedError;
+
+#[derive(Debug)]
+pub struct Flags {
+    map: HashMap<String, Option<String>>,
+}
+
+const FLAGS_WITH_REQUIRED_ARG: [&str; 3] = [
+    "from",
+    "to",
+    "name",
+];
+
+impl Flags {
+    pub fn new() -> Flags {
+        Flags {
+            map: HashMap::new(),
+        }
+    }
+
+    pub fn try_insert<F>(&mut self, flag: &str, get_arg: F) -> Result<(), NormalizedError>
+    where
+        F: FnOnce() -> Option<String>,
+    {
+        let sanitized_flag = &flag.replace('-', "")[..];
+        let long_flag = if sanitized_flag.len() == 1 {
+            match sanitized_flag {
+                "n" => "name",
+                _ => {
+                    println!("{flag} is not a valid short flag.");
+                    return Err(NormalizedError::InvalidFlag(None));
+                }
+            }
+        } else {
+            sanitized_flag
+        };
+
+        let is_arg_required = FLAGS_WITH_REQUIRED_ARG.contains(&long_flag);
+
+        let arg = if is_arg_required {
+            let arg = get_arg();
+            if arg == None {
+                return Err(NormalizedError::RequiredFlagArg(None));
+            }
+            arg
+        } else {
+            None
+        };
+
+        self.map.insert(long_flag.to_string(), arg);
+
+        Ok(())
+    }
+
+    pub fn get(&self, flag: &str) -> &Option<String> {
+        let result = self.map.get(flag);
+
+        if let Some(flag) = result {
+            return flag;
+        } else {
+            return &None;
+        }
+    }
+}
