d320b3b5a5c8f38a59114ad6d67bf9755e1178f7
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2023-05-14 19:59:31 -0300

    chore: create action controller

---
+++ src/gateway/controller/action.rs
@@ -0,0 +1,54 @@
+use crate::gateway::presenter::NormalizedError;
+
+#[derive(Debug)]
+pub enum Action {
+    Swallow,
+    Spit,
+    Treasure(TreasureSubAction),
+}
+
+impl Action {
+    pub fn try_new<F>(action_str: &str, get_subaction: F) -> Result<Self, NormalizedError>
+    where
+        F: FnOnce() -> Option<String>,
+    {
+        if action_str.is_empty() {
+            return Err(NormalizedError::EmptyAction(None));
+        }
+
+        match &action_str.to_lowercase()[..] {
+            "swallow" => Ok(Action::Swallow),
+            "swl" => Ok(Action::Swallow),
+            "spit" => Ok(Action::Spit),
+            "spt" => Ok(Action::Spit),
+            "treasure" => match get_subaction() {
+                Some(s) => {
+                    let subaction = TreasureSubAction::try_new(&s)?;
+                    Ok(Action::Treasure(subaction))
+                }
+                None => Err(NormalizedError::EmptyAction(None)),
+            },
+            _ => Err(NormalizedError::UnknownAction(None)),
+        }
+    }
+}
+
+/* =========================================================
+SUB ACTIONS
+========================================================= */
+
+#[derive(Debug)]
+pub enum TreasureSubAction {
+    List,
+    Name,
+}
+
+impl TreasureSubAction {
+    fn try_new(subaction_str: &str) -> Result<TreasureSubAction, NormalizedError> {
+        match &subaction_str[..] {
+            "list" => Ok(TreasureSubAction::List),
+            "name" => Ok(TreasureSubAction::Name),
+            _ => Err(NormalizedError::EmptyAction(None)),
+        }
+    }
+}
